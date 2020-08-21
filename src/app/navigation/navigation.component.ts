d83a3fd6a47a47b284930659a3803874fa4e9ee4
moemode <email@example.com>
2020-08-21 11:09:18 +0200

    Center project indicator

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -11,11 +11,11 @@ import { map, shareReplay } from 'rxjs/operators';
 export class NavigationComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
