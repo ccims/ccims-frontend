7fffc5242e8eb20168621b94f4210846aca87815
moemode <email@example.com>
2020-08-21 16:50:58 +0200

    Puts sidebar under toolbar

---
+++ src/app/top-toolbar/top-toolbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-top-toolbar',
+  templateUrl: './top-toolbar.component.html',
+  styleUrls: ['./top-toolbar.component.scss']
+})
+export class TopToolbarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+constructor(private breakpointObserver: BreakpointObserver) {}
+
+
+}
