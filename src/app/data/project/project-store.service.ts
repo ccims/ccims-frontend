d3aa1ac6b832880a69ebc409ef1d7257562be40b
moemode <email@example.com>
2020-09-25 14:58:10 +0200

    Signup, login, create project sequence works rudimentarily

--- src/app/data/project/project-store.service.ts
+++ src/app/data/project/project-store.service.ts
@@ -2,23 +2,25 @@ import { Injectable } from '@angular/core';
 import { ApolloQueryResult } from '@apollo/client/core';
 import { Apollo, gql } from 'apollo-angular';
 import { map } from 'rxjs/operators';
-import { CreateProjectGQL, GetAllProjectsGQL, Project } from 'src/generated/graphql';
+import { CreateProjectGQL, CreateProjectInput, GetAllProjectsGQL, Project } from 'src/generated/graphql';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '@app/auth/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectStoreService {
 
-  constructor(private getAllQuery: GetAllProjectsGQL, private createProject: CreateProjectGQL) {
+  constructor(private authService: AuthenticationService, private getAllQuery: GetAllProjectsGQL, private createProject: CreateProjectGQL) {
   }
 
-  create() {
-    this.createProject.mutate().subscribe(({ data}) => {
-      console.log('got data', data);
-    }, (error) => {
-      console.log('there was an error sending the query', error);
-    });
+  create(name: string) {
+    const input: CreateProjectInput = {
+      name,
+      owner: this.authService.currentUserValue.id
+    };
+    return this.createProject.mutate({input});
+  }
 
     /*
     this.apollo.mutate({
@@ -29,7 +31,6 @@ export class ProjectStoreService {
       console.log('there was an error sending the query', error);
     });
     */
-  }
 
   getAll(): Observable<Pick<Project, 'id' | 'name'>[]>{
     return this.getAllQuery.fetch().pipe(
--- src/dialogs/create-project-dialog/create-project-dialog.component.ts
+++ src/dialogs/create-project-dialog/create-project-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProjectStoreService } from '@app/data/project/project-store.service';
+import { CreateProjectInput } from 'src/generated/graphql';
 @Component({
   selector: 'app-create-project-dialog',
   templateUrl: './create-project-dialog.component.html',
@@ -11,7 +13,7 @@ public name: string;
 public loading: boolean;
 public saveFailed: boolean;
 
-  constructor(public dialogRef: MatDialogRef<CreateProjectDialogComponent>) { this.loading = false; }
+  constructor(public dialogRef: MatDialogRef<CreateProjectDialogComponent>, private ps: ProjectStoreService) { this.loading = false; }
   email = new FormControl('', [Validators.required]);
 
   getErrorMessage() {
@@ -26,21 +28,29 @@ public saveFailed: boolean;
   onNoClick(): void {
     // console.log(this.name);
     this.dialogRef.close();
-
   }
+
   onOkClick(): void{
     this.loading = true;
     // console.log(this.url)
-    // delete the timeout function for prod use
+    /*delete the timeout function for prod use
     setTimeout(() => {
       this.loading = false;
       // need good condition (checking save success??)
       if (this.name == ''|| !this.name) {
         this.saveFailed = true;
       } else {
-        this.dialogRef.close(this.name);
       }
     }, 2000);
-
+    */
+    this.ps.create(name).subscribe(({ data}) => {
+      this.loading = false;
+      this.dialogRef.close(this.name);
+      console.log('got data', data);
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+      this.loading = false;
+      this.saveFailed = true;
+    });
   }
 }
