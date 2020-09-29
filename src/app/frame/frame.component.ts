8f91c8d335be3954071d62ccf0b118ac3fc8c255
moemode <email@example.com>
2020-09-29 12:59:12 +0200

    Hides menu button when necessary

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { map, shareReplay } from 'rxjs/operators';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { StateService } from '@app/state.service';
 
 @Component({
@@ -10,16 +10,20 @@ import { StateService } from '@app/state.service';
   styleUrls: ['./frame.component.scss']
 })
 export class FrameComponent implements OnInit {
+  public isProjectSet$ = new BehaviorSubject<boolean>(false);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
     map(result => result.matches),
     shareReplay()
   );
-  constructor(private breakpointObserver: BreakpointObserver) {
+
+  constructor(private breakpointObserver: BreakpointObserver, public ss: StateService) {
+    ss.state$.pipe(
+      map(state =>(state.project != null))
+    ).subscribe(this.isProjectSet$);
   }
 
   ngOnInit(): void {
   }
-
 }
