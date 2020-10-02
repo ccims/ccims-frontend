b7229d06b259c1b267db65a71645819b5504e2e8
Fabian Danner <fabian.danner@gmail.com>
2020-10-02 08:29:04 +0200

    new component

---
+++ src/app/dialogs/create-interface-dialog/create-interface-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-interface-dialog',
+  templateUrl: './create-interface-dialog.component.html',
+  styleUrls: ['./create-interface-dialog.component.scss']
+})
+export class CreateInterfaceDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
