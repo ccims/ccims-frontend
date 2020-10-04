946c849bb7d548e46293fa0954c9136e1f9bc66d
moemode <pfeffer.moritz@gmail.com>
2020-10-04 11:26:56 +0200

    Implements first version of UniqueHashMap

---
+++ src/utils/UniqueHashMap.ts
@@ -0,0 +1,55 @@
+
+class MyMap<Key extends IEquality<Key>, Value> {
+  constructor(entries?: Iterable<[Key, Value]>, private defaultValue?: Value) {
+    if (entries) {
+      for (const entry of entries) {
+        const [key, value] = entry;
+      }
+    }
+  }
+  map: Map<string, [Key, Value]> = new Map();
+  length = 0;
+
+  set(key: Key, value: Value): this {
+    const hashString = key.hashString();
+    const oldValue = this.get(key);
+    if (!oldValue) {
+      this.length++;
+    }
+    this.map.set(hashString, [key, value]);
+    return this;
+  }
+
+
+  has(key: Key): boolean {
+    return this.get(key) !== undefined;
+  }
+
+  getDefault(key: Key): Value | undefined {
+    return this.get(key) || this.defaultValue;
+  }
+
+  /**
+   *
+   */
+  get(key: Key): Value | undefined {
+    const hashString = key.hashString();
+    const oldKeyValue = this.map.get(hashString);
+    if (oldKeyValue) {
+      const [oldKey, value] = oldKeyValue;
+      if (key.equals(oldKey)) {
+        return value;
+      } else {
+        throw new Error(`${key} has same hashString as ${oldKey}.\n
+        It is: "${hashString}". They must be equal, but are not!\n
+        `);
+      }
+    }
+    return undefined;
+  }
+}
+
+interface IEquality<T extends IEquality<T>> {
+  equals(other: this): boolean;
+  hashString(): string;
+}
