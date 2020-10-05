4b3fe1877900e7e4f3394f2ab1c1c5a0693be209
Fabian Danner <fabian.danner@gmail.com>
2020-10-05 15:38:58 +0200

    fixed projectOverview

--- src/app/project-overview/project-overview.component.ts
+++ src/app/project-overview/project-overview.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MockProjectStoreService } from '@app/data/project/mock-project-store.service';
 import { ProjectStoreService } from '@app/data/project/project-store.service';
 import { Interface } from 'readline';
-import { Maybe, Project, User, UserPage } from 'src/generated/graphql';
+
 
 @Component({
   selector: 'app-project-overview',
@@ -11,23 +11,26 @@ import { Maybe, Project, User, UserPage } from 'src/generated/graphql';
   styleUrls: ['./project-overview.component.scss']
 })
 export class ProjectOverviewComponent implements OnInit {
-  private project: any;
+  //@Input() public project: any;
+  public project: any;
+
   public projectId: string;
 
   constructor(private projectStoreService: ProjectStoreService, private route: ActivatedRoute) {
+    this.projectId = this.route.snapshot.paramMap.get('id');
+    this.project = this.projectStoreService.getFullProject(this.projectId).subscribe(project => {this.project = project;
+    });
+
   }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.projectId = this.route.snapshot.paramMap.get('id');
-    this.project = this.projectStoreService.getFullProject(this.projectId).subscribe(project => this.project = project);
 
   }
-
 }
  interface CurrentProject {
-  name: string;
-  id: string;
+  name?: string;
+  id?: string;
   description?: string;
 
 };
