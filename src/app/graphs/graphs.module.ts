1011cff01e5ad0fd2ad4f80e263605ecebda425d
Fabian Danner <fabian.danner@gmail.com>
2020-10-19 07:09:15 +0200

    added pagination

--- src/app/graphs/graphs.module.ts
+++ src/app/graphs/graphs.module.ts
@@ -21,6 +21,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentDetailsComponent } from '../component-details/component-details.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import {  MatPaginatorModule } from '@angular/material/paginator';
 import { IssueListComponent } from '@app/issue-list/issue-list.component';
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
@@ -54,6 +55,7 @@ import {CdkTreeModule} from '@angular/cdk/tree';
     MatSelectModule,
     MatTabsModule,
     MatTableModule,
+    MatPaginatorModule
   ],
   exports: [
     CdkTableModule,
--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -5,7 +5,7 @@ import { GetComponentQuery, Issue } from 'src/generated/graphql';
 import { Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import {AfterViewInit} from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
@@ -15,37 +15,28 @@ export class IssueListComponent implements OnInit {
   public component$: Observable<GetComponentQuery>;
   private component: GetComponentQuery;
   private componentId: string;
-  dataSource;
-  //columnsToDisplay = ['id', 'Title'];
-   columnsToDisplay = ['title', 'author', 'assignees', 'labels', 'category'];
-   example = [
-     {title:"blatitle",createdBy:{displayName:"einautor"},assignees:{nodes:["eine","nocheiner"]},labels:{nodes:["bla","blaaasdasd"]},category:"BUG"},
-     {title:"anderer Titel",createdBy:{displayName:"einautor"},assignees:{nodes:["eine","nocheiner"]},labels:{nodes:["bla","blaaasdasd"]},category:"BUG"},
-     {title:"Nochwas anderes",createdBy:{displayName:"einautor"},assignees:{nodes:["eine","nocheiner"]},labels:{nodes:["bla","blaaasdasd"]},category:"BUG"}
-   ]
+  loading = true;
+  dataSource: MatTableDataSource<any>;
+  columnsToDisplay = ['title', 'author', 'assignees', 'labels', 'category'];
 
-   @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private route: ActivatedRoute, private componentStoreService: ComponentStoreService) {
     this.componentId = this.route.snapshot.paramMap.get('componentId');
-
     this.component$ = this.componentStoreService.getFullComponent(this.componentId);
     this.component$.subscribe(component => {
       this.component = component;
-      this.dataSource = new MatTableDataSource<any>(this.component.node.issues.nodes);
+      this.dataSource = new MatTableDataSource<any>(component.node.issues.nodes);
       this.dataSource.paginator = this.paginator;
+      console.log(this.paginator);
+      this.loading = false;
     });
-   }
-
-  ngOnInit(): void {
-
   }
 
-
+  ngOnInit(): void {
+  }
 
   clickedOnRow(row: any) {
     console.log(row);
-
 
   }
 
