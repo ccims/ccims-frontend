fdb98b70c43c82568e974e88f42dba542ead9188
moemode <pfeffer.moritz@gmail.com>
2020-10-28 12:38:49 +0100

    Add filter boilerplate

--- src/app/graphs/label-search/label-search.component.ts
+++ src/app/graphs/label-search/label-search.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelSearchComponent implements OnInit {
 
-  constructor() { }
+  people$: Observable<Person[]>;
+  peopleLoading = false;
+  peopleInput$ = new Subject<string>();
+  selectedPersons: Person[] = <any>[{ name: 'Karyn Wright' }, { name: 'Other' }];
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+      this.loadPeople();
+  }
+
+  trackByFn(item: Person) {
+      return item.id;
+  }
+
+  private loadPeople() {
+      this.people$ = concat(
+          of([]), // default items
+          this.peopleInput$.pipe(
+              distinctUntilChanged(),
+              tap(() => this.peopleLoading = true),
+              switchMap(term => this.dataService.getPeople(term).pipe(
+                  catchError(() => of([])), // empty list on error
+                  tap(() => this.peopleLoading = false)
+              ))
+          )
+      );
   }
 
 }
