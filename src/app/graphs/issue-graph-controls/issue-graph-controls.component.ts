a515990781f8e379fdf8f981f21334a9e2da2f14
moemode <email@example.com>
2020-12-05 14:16:05 +0100

    Add subscription termination to IssueGraphControlsComponent

--- src/app/graphs/issue-graph-controls/issue-graph-controls.component.ts
+++ src/app/graphs/issue-graph-controls/issue-graph-controls.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StateService } from '@app/state.service';
 import { ActivatedRoute } from '@angular/router';
 import { IssueGraphComponent } from '../issue-graph/issue-graph.component';
 import { IssueCategory } from 'src/generated/graphql';
-import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, ReplaySubject } from 'rxjs';
 import { SelectedCategories } from '../shared';
 import { IssueGraphStateService } from '../../data/issue-graph/issue-graph-state.service';
 import { LabelSearchComponent } from '../label-search/label-search.component';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
 import { FilterState } from '@app/graphs/shared';
 
 @Component({
@@ -16,7 +16,7 @@ import { FilterState } from '@app/graphs/shared';
   templateUrl: './issue-graph-controls.component.html',
   styleUrls: ['./issue-graph-controls.component.scss']
 })
-export class IssueGraphControlsComponent implements AfterViewInit {
+export class IssueGraphControlsComponent implements AfterViewInit, OnDestroy {
   @ViewChild(IssueGraphComponent) issueGraph: IssueGraphComponent;
   @ViewChild(LabelSearchComponent) labelSearch: LabelSearchComponent;
   projectId: string;
@@ -27,6 +27,7 @@ export class IssueGraphControlsComponent implements AfterViewInit {
 
   showRelations = true;
   filter$: BehaviorSubject<FilterState>;
+  private destroy$ = new ReplaySubject(1);
 
   constructor(public dialog: MatDialog, private gs: IssueGraphStateService, private route: ActivatedRoute) {
     this.projectId = this.route.snapshot.paramMap.get('id');
@@ -56,9 +57,12 @@ export class IssueGraphControlsComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     combineLatest([this.selectedCategories$, this.labelSearch.filterSelection$]).pipe(
+      takeUntil(this.destroy$),
       map(([selectedCategories, filterSelection]) => ({ selectedCategories, selectedFilter: filterSelection }))
     ).subscribe(filterState => this.filter$.next(filterState));
-    this.gs.graphDataForFilter(this.filter$, this.issueGraph.reload$).subscribe(
+
+    this.gs.graphDataForFilter(this.filter$, this.issueGraph.reload$).pipe(
+      takeUntil(this.destroy$)).subscribe(
       graphData => {
         this.issueGraph.graphData = graphData;
         this.issueGraph.drawGraph();
@@ -69,4 +73,9 @@ export class IssueGraphControlsComponent implements AfterViewInit {
   setRelationVisibility(): void {
     this.issueGraph.setRelationVisibility(this.showRelations);
   }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
+
 }
