cdaf7bad5d2247a7a66417a7a90495b3305b919b
Till Mayer <till.mayer@web.de>
2021-09-19 16:04:04 +0200

    Add keyboard shortcut to open component properties in new window

--- src/app/graphs/issue-graph/issue-graph.component.ts
+++ src/app/graphs/issue-graph/issue-graph.component.ts
@@ -692,9 +692,6 @@ export class IssueGraphComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   private onNodeClick = (event: CustomEvent) => {
-    // console.log(event.detail.node.x, event.detail.node.y);
-    // return;
-
     event.preventDefault(); // prevent node selection
     const node: Node = event.detail.node;
 
@@ -709,7 +706,7 @@ export class IssueGraphComponent implements OnInit, OnDestroy, AfterViewInit {
     this.redrawByCloseOfComponentDetails = true;
     let contextMenuType: NodeDetailsType = null;
 
-    if (this.isHandset) {
+    if (event.detail.sourceEvent.ctrlKey || this.isHandset) {
       if (node.type === NodeType.Component) {
         this.router.navigate(['./component/', node.id], {relativeTo: this.activatedRoute.parent});
         return;
