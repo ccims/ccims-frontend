4921c8a117126bb867d5cb46711f299d1406750a
Till Mayer <till.mayer@web.de>
2021-09-21 14:29:01 +0200

    Add some documentation to the text display component

--- src/app/text-display/text-display.component.ts
+++ src/app/text-display/text-display.component.ts
@@ -1,16 +1,27 @@
 import {Component, Input} from '@angular/core';
 
+/**
+ * A callback function that is called once the user finishes editing the text.
+ * @param saved true if the user clicked save, false if the user cancelled the editing
+ */
 export type TextDisplayEditCallbackFn = (saved: boolean) => void;
 
+
+/**
+ * The text display component shows text in a labeled box, and if desired, allows the user to edit and save the
+ * text
+ */
 @Component({
   selector: 'app-text-display',
   templateUrl: './text-display.component.html',
   styleUrls: ['./text-display.component.scss']
 })
 export class TextDisplayComponent {
+  /** A callback function. */
   @Input() onEditFinished: TextDisplayEditCallbackFn;
+  /** The title text. */
   @Input() labelText: string;
-
+  /** Sets the content of the text display, and shows the placeholder if the text is empty */
   @Input() set text(value: string) {
     this.value = value;
     this.showPlaceholder = !value || value.length === 0;
@@ -20,7 +31,9 @@ export class TextDisplayComponent {
     return this.value;
   }
 
+  /** If true, the edit button is not shown */
   @Input() readonly: boolean;
+  /** The placeholder will be shown if the content is empty */
   @Input() placeholder = '';
 
   private value;
