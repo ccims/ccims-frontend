7a646d38e92f051d5c9533a8531c912170697550
Till Mayer <till.mayer@web.de>
2021-10-03 19:49:05 +0200

    Coalesce add/remove label timeline items

--- src/app/issue-detail/timeline/timeline.component.ts
+++ src/app/issue-detail/timeline/timeline.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { TimeFormatter } from '@app/issue-detail/TimeFormatter';
-import { Router } from '@angular/router';
-import { IssueTimelineItem } from '../../../generated/graphql-dgql';
-import { DataList } from '@app/data-dgql/query';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {TimeFormatter} from '@app/issue-detail/TimeFormatter';
+import {Router} from '@angular/router';
+import {IssueTimelineItem} from '../../../generated/graphql-dgql';
+import {DataList} from '@app/data-dgql/query';
 import DataService from '@app/data-dgql';
-import { encodeListId, encodeNodeId, ListType, NodeType } from '@app/data-dgql/id';
+import {encodeListId, encodeNodeId, ListType, NodeType} from '@app/data-dgql/id';
+
+export interface CoalescedTimelineItem {
+  user: string;
+  type: string;
+  isCoalesced: boolean;
+  item: Array<IssueTimelineItem> | IssueTimelineItem;
+  time: string;
+}
 
 @Component({
   selector: 'app-timeline',
@@ -13,17 +21,19 @@ import { encodeListId, encodeNodeId, ListType, NodeType } from '@app/data-dgql/i
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit, OnDestroy {
+  static readonly COALESCABLE_EVENTS = new Set(['LabelledEvent', 'UnlabelledEvent']);
 
   // Provides time format functions
   public timeFormatter = new TimeFormatter();
-  timelineItems = [];
+  timelineItems: Array<CoalescedTimelineItem> = [];
   public timelineItems$: DataList<IssueTimelineItem, unknown>;
   public timelineItemsSub: Subscription;
   @Input() issueId: string;
   @Input() projectID: string;
 
   constructor(private dataService: DataService,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit(): void {
     this.requestTimelineItems();
@@ -32,12 +42,99 @@ export class TimelineComponent implements OnInit, OnDestroy {
   requestTimelineItems(): void {
     // Get observeable with all timelineitems for current issue
     this.timelineItems$ = this.dataService.getList(encodeListId({
-      node: { type: NodeType.Issue, id: this.issueId },
+      node: {type: NodeType.Issue, id: this.issueId},
       type: ListType.TimelineItems
     }));
     this.timelineItems$.count = 99999; // FIXME?
 
-    this.timelineItemsSub = this.timelineItems$.subscribe();
+    this.timelineItemsSub = this.timelineItems$.subscribe(value => {
+      this.prepareTimelineItems(value);
+    });
+  }
+
+  prepareTimelineItems(items: Map<string, IssueTimelineItem>): void {
+    let coalescingType = null;
+    let coalesceList = new Array<IssueTimelineItem>();
+    const coalesced: Array<CoalescedTimelineItem> = [];
+
+    // This function adds items from the coalesce list into a coalesced timeline item
+    const finishCoalescing = () => {
+      if (coalesceList.length === 0) {
+        return;
+      }
+
+      const firstItem: any = coalesceList[0];
+      const itemType = firstItem.__typename;
+      const createdBy = firstItem.createdBy.displayName;
+      if (coalesceList.length > 1) {
+        coalesced.push({
+          type: itemType,
+          isCoalesced: true,
+          item: coalesceList,
+          user: createdBy,
+          time: coalesceList[0].createdAt
+        });
+        coalesceList = [];
+      } else if (coalesceList.length === 1) {
+        coalesced.push({
+          type: itemType,
+          isCoalesced: false,
+          item: coalesceList[0],
+          user: createdBy,
+          time: coalesceList[0].createdAt
+        });
+        coalesceList = [];
+      }
+    };
+
+    for (const timelineItem of items.values()) {
+      const itemType = (timelineItem as any).__typename;
+      const createdBy = timelineItem.createdBy.displayName;
+      let stopCoalescing = false;
+
+      if (coalescingType) {
+        // Stop coalescing if user differs or the current item was added 60 seconds after the first one
+        const firstItem = coalesceList[0];
+        stopCoalescing = firstItem.createdBy.id !== timelineItem.createdBy.id ||
+          Math.abs(Date.parse(timelineItem.createdAt) - Date.parse(firstItem.createdAt)) > 60000;
+      }
+
+      if (coalescingType !== itemType || stopCoalescing) {
+        finishCoalescing();
+
+        if (TimelineComponent.COALESCABLE_EVENTS.has(itemType)) {
+          coalescingType = itemType;
+          coalesceList.push(timelineItem);
+        } else {
+          coalescingType = null;
+          coalesced.push({
+            type: itemType,
+            isCoalesced: false,
+            item: timelineItem,
+            user: createdBy,
+            time: timelineItem.createdAt
+          });
+        }
+
+        continue;
+      } else if (coalescingType == null) {
+        coalesced.push({
+          type: itemType,
+          isCoalesced: false,
+          item: timelineItem,
+          user: createdBy,
+          time: timelineItem.createdAt
+        });
+
+        continue;
+      }
+
+      coalesceList.push(timelineItem);
+    }
+
+    // Add remaining items
+    finishCoalescing();
+    this.timelineItems = coalesced;
   }
 
   ngOnDestroy() {
@@ -48,29 +145,27 @@ export class TimelineComponent implements OnInit, OnDestroy {
    * Checks if user self assigned this issue for text representation
    * @param timelineItem
    */
-  selfAssigned(assignedEvent): boolean{
-    if (assignedEvent.createdBy.id === assignedEvent.removedAssignee?.id){
+  selfAssigned(assignedEvent): boolean {
+    if (assignedEvent.createdBy.id === assignedEvent.removedAssignee?.id) {
       return true;
-    }
-    else if (assignedEvent.createdBy.id === assignedEvent.assignee?.id){
+    } else if (assignedEvent.createdBy.id === assignedEvent.assignee?.id) {
       return true;
     }
     return false;
   }
 
   makeCommentId(node) {
-    return encodeNodeId({ type: NodeType.IssueComment, id: node.id });
+    return encodeNodeId({type: NodeType.IssueComment, id: node.id});
   }
 
-  goToComponentDetails(component){
+  goToComponentDetails(component) {
     this.router.navigate(['projects', this.projectID, 'component', component.id]);
   }
 
-  goToLocationDetails(location){
-    if (location.__typename === 'Component'){
+  goToLocationDetails(location) {
+    if (location.__typename === 'Component') {
       this.router.navigate(['projects', this.projectID, 'component', location.id]);
-    }
-    else {
+    } else {
       this.router.navigate(['projects', this.projectID, 'interface', location.id]);
     }
   }
