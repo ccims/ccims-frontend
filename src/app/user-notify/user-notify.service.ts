e6c4b3c538b40002a95a01e465f6230073e91987
Till Mayer <till.mayer@web.de>
2021-11-11 22:17:46 +0100

    Document user notify service

--- src/app/user-notify/user-notify.service.ts
+++ src/app/user-notify/user-notify.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from '@angular/core';
 import {IndividualConfig, ToastrService} from 'ngx-toastr';
 
+/**
+ * This service provides functions for user notification
+ */
 @Injectable({providedIn: 'root'})
 export class UserNotifyService {
+  /** @ignore */
   private errorConfig: Partial<IndividualConfig> = {
     timeOut: 10000,
     closeButton: true,
@@ -10,6 +14,7 @@ export class UserNotifyService {
     enableHtml: true
   };
 
+  /** @ignore */
   private infoConfig: Partial<IndividualConfig> = {
     timeOut: 4000,
     closeButton: true,
@@ -20,6 +25,11 @@ export class UserNotifyService {
   constructor(private toastr: ToastrService) {
   }
 
+  /**
+   * Notify the user that an error occurred
+   * @param message The message to be shown
+   * @param error Optionally, the error. This will be logged in the console, if defined.
+   */
   notifyError(message: string, error?: Error) {
     if (error !== undefined) {
       console.log('Error:', error);
@@ -28,6 +38,10 @@ export class UserNotifyService {
     this.toastr.error(message, 'Error!', this.errorConfig);
   }
 
+  /**
+   * Notify the user
+   * @param message The message to be shown
+   */
   notifyInfo(message: string) {
     this.toastr.info(message, 'Info', this.infoConfig);
   }
