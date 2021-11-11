1c99ed8b0b5dab8d6ebe7644ab8cdc18ca1c4281
Till Mayer <till.mayer@web.de>
2021-11-11 18:24:20 +0100

    Document component context menu

--- src/app/graphs/component-context-menu/component-context-menu.component.ts
+++ src/app/graphs/component-context-menu/component-context-menu.component.ts
@@ -14,24 +14,49 @@ import {
 import {ConnectedPosition, Overlay, OverlayRef} from '@angular/cdk/overlay';
 import {ComponentPortal, PortalInjector} from '@angular/cdk/portal';
 import {IssueGraphComponent} from '@app/graphs/issue-graph/issue-graph.component';
-import {NodeDetailsComponent, NodeDetailsType} from '@app/node-details/node-details.component';
+import {NodeDetailsComponent, NodeDetailsType, NodeUpdatedCallbackFn} from '@app/node-details/node-details.component';
 
-export interface ComponentContextMenuData {
+/**
+ * Interface specifying the data required for the component context menu.
+ * Note that this should not be used directly, instead the ComponentContextMenuService should be used to open a context
+ * menu.
+ */
+interface ComponentContextMenuData {
+  /** Reference to the overlay used to display the context menu */
   overlayRef: OverlayRef;
+  /** The position of the overlay */
   position: ConnectedPosition;
+  /** The project id string */
   projectId: string;
+  /** The node id string */
   nodeId: string;
+  /** The type of node, either interface or component. Controls the content shown in the context menu */
   type: NodeDetailsType;
+  /** A reference to the issue graph */
   graph: IssueGraphComponent;
 }
 
-export const COMPONENT_CONTEXT_MENU_DATA = new InjectionToken<ComponentContextMenuData>('COMPONENT_CONTEXT_MENU_DATA');
+const COMPONENT_CONTEXT_MENU_DATA = new InjectionToken<ComponentContextMenuData>('COMPONENT_CONTEXT_MENU_DATA');
 
+/**
+ * Use this service to create a {@link ComponentContextMenuComponent}.
+ */
 @Injectable({providedIn: 'root'})
 export class ComponentContextMenuService {
   constructor(private overlay: Overlay, private injector: Injector) {
   }
 
+  /**
+   * Open a new component context menu
+   * @param parent The parent of the context menu
+   * @param x The X position relative to the top left corner of the parent
+   * @param y The Y position relative to the top left corner of the parent
+   * @param projectID The id of the project the node belongs to
+   * @param nodeID The id of the node
+   * @param nodeType The type of the node
+   * @param issueGraph A reference to the issue graph
+   * @return A reference to the context menu
+   */
   open(parent: Element,
        x: number, y: number,
        projectID: string,
@@ -69,49 +94,47 @@ export class ComponentContextMenuService {
   }
 }
 
+/**
+ * This component manages the component context menu as well as its content
+ */
 @Component({
   styleUrls: ['component-context-menu.component.scss'],
   templateUrl: './component-context-menu.component.html'
 })
 export class ComponentContextMenuComponent implements AfterViewInit, OnDestroy {
+  /** @ignore */
   private static MIN_WIDTH = 700;
+  /** @ignore */
   private static MIN_HEIGHT = 400;
+  /** @ignore */
   private static LAST_WIDTH = ComponentContextMenuComponent.MIN_WIDTH;
+  /** @ignore */
   private static LAST_HEIGHT = ComponentContextMenuComponent.MIN_HEIGHT;
-
-  width = ComponentContextMenuComponent.LAST_WIDTH;
-  height = ComponentContextMenuComponent.LAST_HEIGHT;
+  /** @ignore */
   private resize = false;
-  nodeDetailsReady: boolean;
-  detailsCallback = (nodeDeleted) => {
-    this.data.graph.reload();
-    if (nodeDeleted) {
-      this.close();
-    }
-  };
 
+  /** Current width of the dialog */
+  width = ComponentContextMenuComponent.LAST_WIDTH;
+  /** Current height of the dialog */
+  height = ComponentContextMenuComponent.LAST_HEIGHT;
+  /** True if the node details component is loaded */
+  nodeDetailsReady: boolean;
+
+  /** @ignore */
   @ViewChild('frame') frame: ElementRef;
 
+  /** @ignore */
   @ViewChild('resizeCorner') set resizeCorner(content: ElementRef) {
     if (content) {
       content.nativeElement.addEventListener('mousedown', () => this.resize = true);
     }
   }
 
+  /** @ignore */
   @ViewChild(NodeDetailsComponent) nodeDetails: NodeDetailsComponent;
 
   constructor(@Inject(COMPONENT_CONTEXT_MENU_DATA) public data: ComponentContextMenuData,
               private changeDetector: ChangeDetectorRef) {
-  }
-
-  updatePosition(x: number, y: number): void {
-    this.data.position.offsetX = x;
-    this.data.position.offsetY = y;
-    this.data.overlayRef.getConfig().positionStrategy.apply();
-  }
-
-  close(): void {
-    this.data.overlayRef.dispose();
   }
 
   ngAfterViewInit() {
@@ -122,15 +145,44 @@ export class ComponentContextMenuComponent implements AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    // TODO: Save in local storage?
     ComponentContextMenuComponent.LAST_WIDTH = this.width;
     ComponentContextMenuComponent.LAST_HEIGHT = this.height;
   }
 
+  /** @ignore */
+  detailsCallback: NodeUpdatedCallbackFn = (nodeDeleted: boolean): void => {
+    this.data.graph.reload();
+    if (nodeDeleted) {
+      this.close();
+    }
+  }
+
+  /**
+   * Update the position of the context menu
+   * @param x The X offset of the top left menu corner relative to the top left corner of the parent
+   * @param y The Y offset of the top left menu corner relative to the top left corner of the parent
+   */
+  updatePosition(x: number, y: number): void {
+    this.data.position.offsetX = x;
+    this.data.position.offsetY = y;
+    this.data.overlayRef.getConfig().positionStrategy.apply();
+  }
+
+  /**
+   * Close the context menu
+   */
+  close(): void {
+    this.data.overlayRef.dispose();
+  }
+
+  /** @ignore */
   @HostListener('window:mouseup')
   private onMouseUp() {
     this.resize = false;
   }
-
+  
+  /** @ignore */
   @HostListener('window:mousemove', ['$event'])
   private onMouseMove(event: MouseEvent) {
     if (!this.resize) {
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,15 +1,31 @@
 import {Validators} from '@angular/forms';
 
+/**
+ * This class provides commonly used validators in CCIMS usable for e.g. input fields
+ */
 export class CCIMSValidators {
+  /**
+   * Ensures that text has the appropriate length to be used as a name
+   */
   static readonly nameValidator = Validators.maxLength(256);
 
+  /**
+   * Ensures that the text is both usable as a name ({@link #nameValidator}) and that the text does not start/end with
+   * a whitespace (white-spaces in the name are allowed)
+   */
   static readonly nameFormatValidator = Validators.compose([
     CCIMSValidators.nameValidator,
     Validators.pattern('([^ ]+ )*([^ ]+)+')
   ]);
 
+  /**
+   * Ensures that the provided text has the correct length and the correct format for an URL
+   */
   // TODO: Verify URL pattern, leaving this out for now for quicker testing
   static readonly urlValidator = Validators.maxLength(655536);
 
+  /**
+   * Ensures that the text has the appropriate length for content
+   */
   static readonly contentValidator = Validators.maxLength(655536);
 }
