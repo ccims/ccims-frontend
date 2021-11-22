ccims/ccims-frontend

commit bf966e9edeaaa565b1237fa146be5c2bbfcaac20

Add more documentation

src/app/issue-detail/timeline/timeline.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, Input, ViewChild} from '@angular/core';
-import {TimeFormatter} from '@app/issue-detail/TimeFormatter';
+import {TimeFormatter} from '@app/issue-detail/time-formatter';
 import {Router} from '@angular/router';
 import {IssueTimelineItem} from '../../../generated/graphql-dgql';
 import {DataList} from '@app/data-dgql/query';

src/app/components/issue-item/issue-item.component.ts
@@ -20,6 +20,7 @@ export class IssueItemComponent {
 
   constructor(private router: Router) {}
 
+  /** Returns the link URL for the issue. */
   getIssueLink() {
     return this.router.serializeUrl(this.router.createUrlTree(
       ['/projects', this.projectId, 'issues', this.issue.id]

src/app/components/issue-location/issue-location.component.ts
@@ -19,10 +19,12 @@ export class IssueLocationComponent {
   constructor(private router: Router) {
   }
 
+  /** Returns true if the location refers to a component. */
   isComponent(): boolean {
     return (this.location as any).__typename === 'Component';
   }
 
+  /** Navigates to the location's detail page. */
   goToLocationDetails(): void {
     if (this.isComponent()) {
       this.router.navigate(['projects', this.projectId, 'component', this.location.id]);

src/app/components/set-editor/set-editor-dialog.component.ts
@@ -36,16 +36,26 @@ export interface SetMultiSource {
  * This is an internal component used to load data from multiple sources and through a layer of indirection (also see SetMultiSource).
  */
 class MultiSourceList<T, F> {
+  /** A DataList that loads the value of sourceNodes, if it's a ListId. */
   public sourceNodeList?: DataList<{ __typename: string }, unknown>;
+  /** @ignore */
   public sourceNodeListSub?: Subscription;
+  /** The list of nodes specified in sourceNodes, if it's a NodeId[]. */
   public staticSourceNodeList?: NodeId[];
+  /** List of all sources that will be included in the results. */
   public sources: Map<ListIdEnc, DataList<T, F>> = new Map();
+  /** @ignore */
   public sourceSubs: Map<ListIdEnc, Subscription> = new Map();
   /** Max number of items in results. */
   public limit = 10;
+  /** Current results. */
   public results?: T[];
   /** If true, there are more than `limit` items in the source data. */
   public hasMore = false;
+  /**
+   * Current search query. Used to rank results by relevance.
+   * The filters are computed separately! Use {@link #setFilter} to set both simultaneously.
+   */
   public query = '';
 
   constructor(public spec: SetMultiSource, public scoreKeys: string[], private dataService: DataService) {
@@ -58,6 +68,7 @@ class MultiSourceList<T, F> {
     this.update();
   }
 
+  /** Creates a new MultiSourceList that actually just loads a single list. */
   static fromSingleList<T, F>(list: ListId, scoreKeys: string[], dataService: DataService) {
     return new this<T, F>({ staticSources: [list] }, scoreKeys, dataService);
   }
@@ -65,7 +76,7 @@ class MultiSourceList<T, F> {
   /** Updates lists. */
   update() {
     const newSourceSet = new Set<ListIdEnc>();
-    for (const [id, node] of this.sourceNodeList?.current?.entries() || []) {
+    for (const id of this.sourceNodeList?.current?.keys() || []) {
       const nodeId = decodeNodeId(id);
       newSourceSet.add(encodeListId(this.spec.listFromNode(nodeId)));
     }
@@ -165,6 +176,7 @@ class MultiSourceList<T, F> {
   }
 }
 
+/** Types of item operations that may be made available. */
 export type ItemOps = 'none' | 'edit' | 'create-edit' | 'create-edit-delete';
 
 export interface SetEditorDialogData<T, F> {

src/app/components/user-item/user-item.component.ts
@@ -8,6 +8,7 @@ import { User } from 'src/generated/graphql-dgql';
   styleUrls: ['./user-item.component.scss']
 })
 export class UserItemComponent {
+  /** The user data. */
   @Input() user: User;
   /** Will show only the display name or username. */
   @Input() short = false;

src/app/data-dgql/id.ts
@@ -4,7 +4,8 @@ export type NodeIdEnc = string;
 export type ListIdEnc = string;
 
 /**
- * API node types. The names should match the respective value of __typename for a given node.
+ * Backend API node types.
+ * The names should match the respective value of `__typename` in the GraphQL data for any given node.
  */
 export enum NodeType {
   Root,
@@ -20,7 +21,9 @@ export enum NodeType {
 }
 
 /**
- * Returns the NodeType for a given __typename value.
+ * Returns the NodeType for a given `__typename` value.
+ * `__typename` is a tag found in GraphQL data.
+ *
  * @param typename the __typename value
  */
 export function nodeTypeFromTypename(typename: string) {
@@ -29,6 +32,9 @@ export function nodeTypeFromTypename(typename: string) {
 
 /**
  * Uniquely identifies a node.
+ *
+ * Node IDs are composed of a {@link #type} and an {@link #id}.
+ * The {@link #id} corresponds to the ID used in the backend API.
  */
 export interface NodeId {
   /**
@@ -36,11 +42,15 @@ export interface NodeId {
    */
   type: NodeType;
   /**
-   * The ID of this node. This is the same as the node ID in the API.
+   * The ID of this node. This is the same as the node ID in the backend API.
    */
   id: string;
 }
 
+/**
+ * See {@link encodeNodeId}.
+ * @param id the encoded node ID
+ */
 export function decodeNodeId(id: NodeIdEnc): NodeId {
   if (!id) {
     throw new Error('Could not decode node ID: no id given');
@@ -49,14 +59,26 @@ export function decodeNodeId(id: NodeIdEnc): NodeId {
   return { type: NodeType[parts[0]], id: parts[1] };
 }
 
+/**
+ * Encodes a {@link NodeId} into a string.
+ * This is due to a limitation in Javascript where objects cannot be `==` compared with each other
+ * apart from identity. Encoding them as a string allows `NodeId`s to be used as object or `Map` keys.
+ *
+ * @param nd the node ID
+ */
 export function encodeNodeId(nd: NodeId): NodeIdEnc {
   return NodeType[nd.type] + '/' + nd.id;
 }
 
+/** The ID of the root node. */
 export const ROOT_NODE = { type: NodeType.Root, id: '' };
 
+/** The special ID of the current user node. */
 export const CURRENT_USER_NODE = { type: NodeType.User, id: 'self' };
 
+/**
+ * All list types represent their respective list queries in the backend API.
+ */
 export enum ListType {
   Projects,
   Components,
@@ -76,6 +98,23 @@ export enum ListType {
 
 /**
  * Uniquely identifies a list of items.
+ *
+ * Lists are always attached to a specific node.
+ * For example, a list of issues may be attached to a project or a component.
+ *
+ * Lists that do not have a specific parent node in the backend API are attached to the
+ * {@link ROOT_NODE}.
+ *
+ * #### Examples
+ * The list of all issues on a component:
+ * ```ts
+ * { node: { type: NodeType.Component, id: '...' }, type: ListType.Issues }
+ * ```
+ *
+ * The list of linked issues on an issue:
+ * ```ts
+ * { node: { type: NodeType.Issue, id: '...' }, type: ListType.LinkedIssues }
+ * ```
  */
 export interface ListId {
   /**
@@ -100,6 +139,10 @@ export interface ListParams<F> {
   filter?: F;
 }
 
+/**
+ * See {@link decodeListId}.
+ * @param id an encoded List ID
+ */
 export function decodeListId(id: ListIdEnc): ListId {
   if (!id) {
     throw new Error('Could not decode list ID: no id given');
@@ -108,6 +151,12 @@ export function decodeListId(id: ListIdEnc): ListId {
   return { node: decodeNodeId(parts[0]), type: ListType[parts[1]] };
 }
 
+/**
+ * Encodes a List ID into a string.
+ *
+ * See {@link encodeNodeId} for more details.
+ * @param ld a List ID
+ */
 export function encodeListId(ld: ListId): ListIdEnc {
   return encodeNodeId(ld.node) + '#' + ListType[ld.type];
 }

src/app/data-dgql/load.ts
@@ -29,39 +29,6 @@ const nodeQueries: NodeQueries = {
   [NodeType.Label]: (i, id) => i.q.issues.getLabel(id).then(data => data.node)
 };
 
-/** Loads a node. */
-export const queryNode = (q: QueriesService) => async <T>(nodeId: NodeId): Promise<T> => {
-  const { type, id } = nodeId;
-
-  if (!nodeQueries[type]) {
-    throw new Error(`${NodeType[type]} cannot be loaded directly`);
-  }
-
-  const i = { q };
-  return (await nodeQueries[type](i, id)) as T;
-};
-
-/** Converts ListParams to GraphQL parameters. */
-function listParams<F>(params: ListParams<F>) {
-  const output: any = {};
-  if (params.forward) {
-    output.first = params.count;
-  } else {
-    output.last = params.count;
-  }
-  if (params.cursor) {
-    if (params.forward) {
-      output.after = params.cursor.id;
-    } else {
-      output.before = params.cursor.id;
-    }
-  }
-  if (params.filter) {
-    output.filterBy = params.filter;
-  }
-  return output;
-}
-
 type ListQueryInput = {
   q: QueriesService,
   c: NodeCache,
@@ -206,6 +173,39 @@ const listQueries: ListQueries = {
       }))
   }
 };
+
+/** Loads a node. */
+export const queryNode = (q: QueriesService) => async <T>(nodeId: NodeId): Promise<T> => {
+  const { type, id } = nodeId;
+
+  if (!nodeQueries[type]) {
+    throw new Error(`${NodeType[type]} cannot be loaded directly`);
+  }
+
+  const i = { q };
+  return (await nodeQueries[type](i, id)) as T;
+};
+
+/** Converts ListParams to GraphQL parameters. */
+function listParams<F>(params: ListParams<F>) {
+  const output: any = {};
+  if (params.forward) {
+    output.first = params.count;
+  } else {
+    output.last = params.count;
+  }
+  if (params.cursor) {
+    if (params.forward) {
+      output.after = params.cursor.id;
+    } else {
+      output.before = params.cursor.id;
+    }
+  }
+  if (params.filter) {
+    output.filterBy = params.filter;
+  }
+  return output;
+}
 
 export type ListResult<T> = {
   totalCount: number,

src/app/data-dgql/mutate.ts
@@ -3,15 +3,20 @@ import { QueriesService } from '@app/data-dgql/queries/queries.service';
 import { encodeNodeId, ListId, ListType, NodeId, NodeType } from '@app/data-dgql/id';
 import { CreateIssueInput, IssueCategory } from '../../generated/graphql-dgql';
 
+/**
+ * Contains functions for mutating backend data and automatically invalidating local caches.
+ */
 export class Mutations {
   constructor(private qs: QueriesService, private nc: NodeCache, private invalidateLists: (id: ListType | ListId) => void) {}
 
+  /** Invalidates a node. */
   invalidateNode(id: NodeId) {
     if (this.nc.nodes.has(encodeNodeId(id))) {
       this.nc.getNode(id).loadDebounced();
     }
   }
 
+  /** Inserts new data into a node. */
   updateNode<T>(id: NodeId, data: unknown) {
     this.nc.getNode(id).insertResult(data);
   }

commit 76303881d0daaa3f51252c2351325104fbf1e3b9

Fix broken timeline (regression in bf966e9)

src/app/issue-detail/timeline/timeline.component.ts
@@ -81,7 +81,7 @@ export class TimelineComponent implements AfterViewInit {
   /**
    * The id of the corresponding issue for which the timeline is shown
    */
-  @Input() issueId: string;
+  @Input() issueId: NodeId;
   /**
    * The id of the project in which the issue is listed
    */
@@ -110,7 +110,7 @@ export class TimelineComponent implements AfterViewInit {
 
     // gets an observable with all timeline items for thecurrent issue
     this.timelineItems$ = this.dataService.getList({
-      node: {type: NodeType.Issue, id: this.issueId},
+      node: this.issueId,
       type: ListType.TimelineItems
     });
 